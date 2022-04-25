0d60c5db808c6cb10739aaa526ec51395b8abc76
abel laurente <abellaurente@hotmail.com>
2022-04-24 23:34:33 -0500

    con clasificador new

--- src/base.js
+++ src/base.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'abel',
-    password: 'Indira2475',
+    password: '@bl3009',
     database: 'siga'
 });
 
--- src/routes/api.js
+++ src/routes/api.js
@@ -24,6 +24,16 @@ router.get('/api/programas', (req, res) => {
 });
 });
 
+router.get('/api/clasificador', (req, res) => {
+    con.query('select distinct(cod_clasificador), clasificador from kit order by cod_clasificador', (err, rows, fields) => {
+    if (!err) {
+        res.json(rows);
+    } else {
+        console.log(err);
+    }
+});
+});
+
 router.get('/api/metas', (req, res) => {
     con.query('select distinct(cod_meta), meta from kit order by cod_meta', (err, rows, fields) => {
     if (!err) {
@@ -89,10 +99,32 @@ router.get('/api/kit_por_programa_meta/:prg/:met', (req, res) => {
 });
 });
 
+router.get('/api/kit_por_meta_tarea/:met/:tar', (req, res) => {
+    const { met,tar } = req.params;
+    con.query('select cod_item_fam, nom_item_fam, nivel, tipo_bien, tipo_calculo, meta, sub_finalidad, clasificador from kit where cod_meta = ? and cod_subfin=? order by cod_programa, cod_meta, sub_finalidad limit 0,500', [met, tar], (err, rows, fields) => {
+    if (!err) {
+        res.json(rows);
+    } else {
+        console.log(err);
+    }
+});
+});
+
 
 router.get('/api/kit_por_programa_meta_cadena/:prg/:met/:cad', (req, res) => {
     const { prg,met,cad } = req.params;
     con.query('select cod_item_fam, nom_item_fam, nivel, tipo_bien, tipo_calculo, meta, sub_finalidad, clasificador from kit where cod_programa = ? and cod_meta=? and nom_item_fam like concat("%",?,"%") order by cod_programa, cod_meta, sub_finalidad limit 0,500', [prg, met, cad], (err, rows, fields) => {
+    if (!err) {
+        res.json(rows);
+    } else {
+        console.log(err);
+    }
+});
+});
+
+router.get('/api/kit_por_meta_tarea_cadena/:met/:tar/:cad', (req, res) => {
+    const { met,tar,cad } = req.params;
+    con.query('select cod_item_fam, nom_item_fam, nivel, tipo_bien, tipo_calculo, meta, sub_finalidad, clasificador from kit where cod_meta = ? and cod_subfin=? and nom_item_fam like concat("%",?,"%") order by cod_programa, cod_meta, sub_finalidad limit 0,500', [met,tar,cad], (err, rows, fields) => {
     if (!err) {
         res.json(rows);
     } else {
